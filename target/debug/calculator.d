26f39430b8620dc484b3fd3ca4c11fac92a86e05
jinderbrar <40489378+jinderbrar@users.noreply.github.com>
2023-05-07 20:08:41 +0000

    Initial commit

---
+++ target/debug/calculator.d
@@ -0,0 +1,1 @@
+/home/runner/CLI-Calculator/target/debug/calculator: /home/runner/CLI-Calculator/src/main.rs
---
+++ target/debug/deps/calculator-ebfe01fafecbb33c.d
@@ -0,0 +1,5 @@
+/home/runner/CLI-Calculator/target/debug/deps/calculator-ebfe01fafecbb33c.rmeta: src/main.rs
+
+/home/runner/CLI-Calculator/target/debug/deps/calculator-ebfe01fafecbb33c.d: src/main.rs
+
+src/main.rs:
